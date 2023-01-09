a4a14cbfe361ec7f19c00bc869668bb9e2a8160a
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-01-09 19:00:40 +0100

    Update change icon when selecting different modes

--- resources/js/dark-mode.js
+++ resources/js/dark-mode.js
@@ -6,12 +6,14 @@
  * Modified.
  */
 
-(() => {
+(() =>
+{
     'use strict'
   
     const storedTheme = localStorage.getItem('theme')
   
-    const getPreferredTheme = () => {
+    const getPreferredTheme = () =>
+    {
       if (storedTheme) {
         return storedTheme
       }
@@ -19,22 +21,44 @@
       return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
   
-    const setTheme = function (theme) {
+    const setTheme = function (theme)
+    {
+      
       if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Dark Mode
         document.documentElement.setAttribute('data-bs-theme', 'dark')
       } else {
+        // Light Mode
         document.documentElement.setAttribute('data-bs-theme', theme)
       }
+    }
+
+    var setTheme00 = function (theme)
+    {
+      var a
+      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Dark Mode
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        a = document.getElementById('navBrand').src ='https://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try.svg'
+      } else {
+        // Light Mode
+        document.documentElement.setAttribute('data-bs-theme', theme)        
+        a  = document.getElementById('navBrand').src ='https://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try_white.svg'
+        console.log(a);
+      }
+      console.log(a);
     }
   
     setTheme(getPreferredTheme())
   
-    const showActiveTheme = theme => {
+    const showActiveTheme = theme =>
+    {
       const activeThemeIcon = document.querySelector('.theme-icon-active')
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
       const svgOfActiveBtn = btnToActive.querySelector('i').getAttribute('data-bs-theme-icon')
   
-      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+      document.querySelectorAll('[data-bs-theme-value]').forEach(element =>
+      {
         element.classList.remove('active')
       })
   
@@ -44,21 +68,24 @@
       activeThemeIcon.classList.add(svgOfActiveBtn)
     }
   
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () =>
+    {
       if (storedTheme !== 'light' || storedTheme !== 'dark') {
-        setTheme(getPreferredTheme())
+        setTheme00(getPreferredTheme())
       }
     })
   
-    window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('DOMContentLoaded', () =>
+    {
       showActiveTheme(getPreferredTheme())
   
       document.querySelectorAll('[data-bs-theme-value]')
         .forEach(toggle => {
-          toggle.addEventListener('click', () => {
+          toggle.addEventListener('click', () =>
+          {
             const theme = toggle.getAttribute('data-bs-theme-value')
             localStorage.setItem('theme', theme)
-            setTheme(theme)
+            setTheme00(theme)
             showActiveTheme(theme)
           })
         })
--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="http://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try.svg" alt="Dart a Web-App Logo" width="100px" style="transform:rotate(-2deg);">
+            <img src="http://media.maximilian-mewes.de/project/bl/blubber_lounge_rebrand_try.svg" id="navBrand" alt="Dart a Web-App Logo" width="100px" style="transform:rotate(-2deg);">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
