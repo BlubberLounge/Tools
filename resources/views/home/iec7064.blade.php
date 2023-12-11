9616974f5061ae5642b575bc449a608dfabda701
Maximilian Mewes <mewes.maximilian@gmx.de>
2023-12-11 19:27:49 +0100

    Update IEC7064 ui ✨

--- resources/views/home/iec7064.blade.php
+++ resources/views/home/iec7064.blade.php
@@ -6,24 +6,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col">
-            <input class="input" id="inputString" placeholder="Enter string here">
+    <div class="row justify-center">
+        <div class="col-5">
+            <div class="mb-3">
+                <label for="inputString" class="form-label">Input</label>
+                <input type="text" class="form-control" id="inputString" placeholder="Enter string here">
+            </div>
+            <div class="mb-3">
+                <label for="outputChecksum" class="form-label">Checksum</label>
+                <input type="text" class="form-control" id="outputChecksum" placeholder="checksum" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="outputWholeString" class="form-label">Input + Checksum</label>
+                <input type="text" class="form-control" id="outputWholeString" placeholder="input + checksum" readonly>
+            </div>
+            <div class="mb-3">
+                <p id="isValid">
+                    unkown
+                </p>
+            </div>
         </div>
-        <div class="col">
+        <div class="col-5">
             <button id="btnValidate" class="btn btn-primary"> validate </button>
             <button id="btnGenerate" class="btn btn-primary"> Generate </button>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <input type="text" class="input" id="outputChecksum" disabled>
-        </div>
-        <div class="col">
-            <input type="text" class="input" id="outputWholeString" disabled>
-        </div>
-        <div class="col">
-            <p id="isValid">unkown</p>
         </div>
     </div>
 </div>
